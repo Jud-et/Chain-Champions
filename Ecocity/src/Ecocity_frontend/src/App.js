59577cc497794bbd38c1d9f0373c7833e94b53b5
jan-ice17 <judysjewel17@gmail.com>
2024-09-01 17:35:49 +0300

    src file moved to render html files

--- Ecocity/src/Ecocity_frontend/src/App.js
+++ Ecocity/src/Ecocity_frontend/src/App.js
@@ -1,3 +1,4 @@
+import { html, render } from 'lit-html';
 import logo from './logo2.svg';
 import { Ecocity_backend } from '../../declarations/Ecocity_backend';
 
@@ -16,19 +17,7 @@ class App {
   };
 
   #render() {
-    let body = html`
-      <main>
-        <img src="${logo}" alt="DFINITY logo" />
-        <br />
-        <br />
-        <form action="#">
-          <label for="name">Enter your name: &nbsp;</label>
-          <input id="name" alt="Name" type="text" />
-          <button type="submit">Click Me!</button>
-        </form>
-        <section id="greeting">${this.greeting}</section>
-      </main>
-    `;
+   
     render(body, document.getElementById('root'));
     document
       .querySelector('form')
